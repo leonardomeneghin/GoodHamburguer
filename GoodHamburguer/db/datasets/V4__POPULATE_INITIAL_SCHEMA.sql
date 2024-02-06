21b65bb2599c9f2f17f230f9eac593c5d5802264
Leo <leonardozenni@gmail.com>
2024-02-06 05:47:14 -0300

    add: migrations config

---
+++ GoodHamburguer/db/datasets/V4__POPULATE_INITIAL_SCHEMA.sql
@@ -0,0 +1,15 @@
+﻿--To be more fast, i will add the all needed data here.
+--Products
+INSERT INTO [tb_products] ([name], [price])
+	VALUES  ('X Burguer', 5.00),
+			('X Egg', 4.50),
+			('X Bacon', 7.00),
+			('Fries', 2.00),
+			('Soft drink', 2.50);
+
+INSERT INTO [tb_item_type] ([item_type_name])
+	VALUES	('sandwich'),
+			('soft drink'),
+			('fries');
+
+
---
+++ GoodHamburguer/db/migrations/V3__CREATE_TB_INITIAL_SCHEMA.sql
@@ -0,0 +1,41 @@
+﻿--Define Orders
+CREATE TABLE [tb_orders] (
+  [id_order] INT IDENTITY(1,1) NOT NULL,
+  [date_order] DATETIME,
+  [total_price] DECIMAL(9, 4) DEFAULT 0
+
+);
+--orders PK
+ALTER TABLE [tb_orders] ADD CONSTRAINT [PK_orders] PRIMARY KEY CLUSTERED ([id_order] ASC);
+
+--Define ordered_item
+CREATE TABLE [tb_item_ordered](
+	[id_item_ordered] INT IDENTITY(1,1) NOT NULL,
+	[id_order] INT NOT NULL,
+	[id_product] INT NOT NULL,
+);
+
+--ordered item PK and FK to item ordered
+ALTER TABLE [tb_item_ordered] ADD CONSTRAINT [PK_id_item_ordered] PRIMARY KEY CLUSTERED ([id_item_ordered] ASC)
+ALTER TABLE [tb_item_ordered] ADD CONSTRAINT [FK_tb_orders_tb_item_ordered] FOREIGN KEY ([id_order]) REFERENCES [tb_orders]
+
+--product table, pk and fk to item ordered.
+CREATE TABLE [tb_products](
+	[id_product] INT IDENTITY(1,1) NOT NULL,
+	[name] VARCHAR(100) NOT NULL,
+	[price] DECIMAL(9, 4) DEFAULT 0,
+	[id_item_type] INT NOT NULL,
+	
+);
+
+ALTER TABLE [tb_products] ADD CONSTRAINT [PK_id_product] PRIMARY KEY CLUSTERED ([id_product] ASC);
+ALTER TABLE [tb_item_ordered] ADD CONSTRAINT [FK_tb_products_tb_item_ordered] FOREIGN KEY ([id_product]) REFERENCES [tb_products];
+
+--item type table, PK and FK to product table
+CREATE TABLE [tb_item_type](
+	[id_item_type] INT IDENTITY(1,1) NOT NULL,
+	[item_type_name] VARCHAR(124) NOT NULL
+);
+
+ALTER TABLE [tb_item_type] ADD CONSTRAINT [PK_id_item_type] PRIMARY KEY CLUSTERED ([id_item_type] ASC)
+ALTER TABLE [tb_products] ADD CONSTRAINT [FK_tb_item_type_tb_products] FOREIGN KEY (id_item_type) REFERENCES [tb_item_type]
