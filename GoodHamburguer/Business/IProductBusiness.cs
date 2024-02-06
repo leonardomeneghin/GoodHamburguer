13859ba6347909383cce470051ccbd53ee83c011
Leo <leonardozenni@gmail.com>
2024-02-06 05:48:40 -0300

    add: dependency injection

---
+++ GoodHamburguer/Business/IProductBusiness.cs
@@ -0,0 +1,11 @@
+﻿using GoodHamburguerAPI.Model;
+
+namespace GoodHamburguerAPI.Business
+{
+    public interface IProductBusiness
+    {
+        public List<Product> ListAllMenuItems();
+        public List<Product> ListSandwiches();
+        public List<Product> ListExtras();
+    }
+}
